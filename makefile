c7042c40d0a655b5406814eea9f9d5cb6176f053
marc2h9 <marcverstegen09@gmail.com>
2025-02-25 16:46:20 +0100

    Updated Files

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 CC =g++
 CFLAGS =-Wall -Wextra -ggdb
+LIBDIRS = $(wildcard lib/*)
+INCLUDES = $(addprefix -I $(LIBDIRS))
 
 main: src/main.cpp
-	$(CC) $(CFLAGS) -o bin/main src/main.cpp
+	$(CC) $(CFLAGS) -o bin/main src/main.cpp include/glad/glad.c -lglfw -lGL
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+void frame_buffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
 int main() {
-  std::cout << "Hello, World";
+  // Initialization
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creating Window
+  GLFWwindow *window = glfwCreateWindow(800, 600, "test", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "GLFW failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Loading GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Setting Viewport
+  glViewport(0, 0, 800, 600);
+  void frame_buffer_size_callback(GLFWwindow *window, int width, int height);
+
+  glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+  while(!glfwWindowShouldClose(window)) {
+    // Render Loop
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
